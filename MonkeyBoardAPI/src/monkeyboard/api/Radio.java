c123fe5c216de6ea7ccad3ce6b79fd438131d033
miltonollie <miltonollie@d210e15f-bb6c-4fcc-83d8-0fb3a06c2c5b>
2013-03-21 07:55:30 +0000

    Checkin

---
+++ MonkeyBoardAPI/src/monkeyboard/api/Radio.java
@@ -0,0 +1,76 @@
+package monkeyboard.api;
+
+import java.util.Scanner;
+
+public class Radio {
+
+	private static class LineReader extends Thread {
+		String line;
+
+		public void run() {
+			Scanner scanner = new Scanner(System.in);
+			while (true) 
+				setLine(scanner.next());
+		}
+
+		synchronized String getLine() {
+			String copy = line;
+			line = null;
+			return copy;
+		}
+
+		synchronized void setLine(String line) {
+			this.line = line;
+		}
+	}
+
+	public static void main(String[] args) {
+		KeyStoneAPI kapi = new KeyStoneAPI();
+		System.out.println("Fuck off");
+		byte volume = 15;
+		if (kapi.findAndOpen()) {
+			System.out.println("Port open");
+			int totalPro = (int)kapi.getTotalPrograms();
+			int dabProg = 48;
+			System.out.println("Found ["+totalPro+"] programmes...");
+			for (int i=0; i<totalPro; i++) {
+				System.out.println("Found - "+kapi.getProgramName(RadioMode.DAB, i, false));
+			}
+			kapi.setVolume(volume);
+			kapi.setStereoMode(true);
+			kapi.playStream(RadioMode.DAB, dabProg);
+
+			LineReader reader = new LineReader();
+			reader.start();
+			System.out.println("Now playing - "+kapi.getProgramName(RadioMode.DAB, dabProg, false));
+			while (true) {
+				PlayStatus stat = kapi.getPlayStatus();
+				if (stat == PlayStatus.PLAYING) {
+					String s = kapi.getProgramText();
+					if (s != null) {
+						System.out.println("Text - "+s);
+						System.out.println("Signal - "+kapi.getSignalStrength());
+					}
+					String cmd = reader.getLine();
+					if (cmd != null) {
+						if ("n".equals(cmd)) {
+							kapi.nextStream();
+							System.out.println("Now playing - "+kapi.getProgramName(RadioMode.DAB, ++dabProg%totalPro, false));
+						} else if ("p".equals(cmd)) {
+							kapi.prevStream();
+							System.out.println("Now playing - "+kapi.getProgramName(RadioMode.DAB, --dabProg%totalPro, false));
+						} else if ("+".equals(cmd)) {
+							kapi.volumePlus();
+						} else if ("-".equals(cmd)) {
+							kapi.volumeMinus();
+						} else if ("m".equals(cmd)) {
+							kapi.volumeMute();
+						} else {
+							System.out.println("You fucking queer");
+						}
+					}
+				}
+			}
+		}
+	}
+}
