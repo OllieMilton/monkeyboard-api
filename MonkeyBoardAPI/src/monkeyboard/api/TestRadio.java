49c481b43f2f732ba70f7dc4d849798d71b025f0
miltonollie <miltonollie@d210e15f-bb6c-4fcc-83d8-0fb3a06c2c5b>
2013-10-21 19:14:56 +0000

    Fooking update

--- MonkeyBoardAPI/src/monkeyboard/api/TestRadio.java
+++ MonkeyBoardAPI/src/monkeyboard/api/TestRadio.java
@@ -32,40 +32,40 @@ public class TestRadio {
 		if (kapi.findAndOpen()) {
 			System.out.println("Port open");
 			int totalPro = (int)kapi.getTotalPrograms();
-			int dabProg = 48;
+			int dabProg = 0;
 			System.out.println("Found ["+totalPro+"] programmes...");
 			for (int i=0; i<totalPro; i++) {
 				System.out.println("Found - "+kapi.getProgramName(RadioMode.DAB, i, false));
 			}
 			kapi.setVolume(volume);
 			kapi.setStereoMode(true);
-			kapi.playStream(RadioMode.DAB, dabProg);
-
-			LineReader reader = new LineReader();
-			reader.start();
-			System.out.println("Now playing - "+kapi.getProgramName(RadioMode.DAB, dabProg, false));
-			while (true) {
-				PlayStatus stat = kapi.getPlayStatus();
-				if (stat == PlayStatus.PLAYING) {
-					String s = kapi.getProgramText();
-					if (s != null) {
-						System.out.println("Text - "+s);
-						System.out.println("Signal - "+kapi.getSignalStrength());
-					}
-					String cmd = reader.getLine();
-					if (cmd != null) {
-						if ("n".equals(cmd)) {
-							kapi.nextStream();
-							System.out.println("Now playing - "+kapi.getProgramName(RadioMode.DAB, ++dabProg%totalPro, false));
-						} else if ("p".equals(cmd)) {
-							kapi.prevStream();
-							System.out.println("Now playing - "+kapi.getProgramName(RadioMode.DAB, --dabProg%totalPro, false));
-						} else if ("+".equals(cmd)) {
-							kapi.volumePlus();
-						} else if ("-".equals(cmd)) {
-							kapi.volumeMinus();
-						} else if ("m".equals(cmd)) {
-							kapi.volumeMute();
+			if (kapi.playStream(RadioMode.DAB, dabProg)) {
+				LineReader reader = new LineReader();
+				reader.start();
+				System.out.println("Now playing - "+kapi.getProgramName(RadioMode.DAB, dabProg, false));
+				while (true) {
+					PlayStatus stat = kapi.getPlayStatus();
+					if (stat == PlayStatus.PLAYING) {
+						String s = kapi.getProgramText();
+						if (s != null) {
+							System.out.println("Text - "+s);
+							System.out.println("Signal - "+kapi.getSignalStrength());
+						}
+						String cmd = reader.getLine();
+						if (cmd != null) {
+							if ("n".equals(cmd)) {
+								kapi.nextStream();
+								System.out.println("Now playing - "+kapi.getProgramName(RadioMode.DAB, ++dabProg%totalPro, false));
+							} else if ("p".equals(cmd)) {
+								kapi.prevStream();
+								System.out.println("Now playing - "+kapi.getProgramName(RadioMode.DAB, --dabProg%totalPro, false));
+							} else if ("+".equals(cmd)) {
+								kapi.volumePlus();
+							} else if ("-".equals(cmd)) {
+								kapi.volumeMinus();
+							} else if ("m".equals(cmd)) {
+								kapi.volumeMute();
+							}
 						}
 					}
 				}
